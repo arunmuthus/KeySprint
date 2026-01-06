a01a9d5bf189f89aa4c4c3a79cd1911f7f8c205f
arunmuthus <arunmuthu.profiles@gmail.com>
2026-01-06 15:59:28 +0530

    added logo with animation

---
+++ frontEnd/src/Auth.jsx
@@ -0,0 +1,20 @@
+import  './css/Auth.css';
+export const Auth = () => {
+    return(
+        <div className="auth-container">
+
+        <div className="logo">
+            <div className="a k"></div>
+            <div className="a s"></div>
+            <div className="a l"></div>
+            <h1><h1>Key</h1>
+            <h1>Sprint</h1></h1>
+            <h2>Lets Type Better Together</h2>     
+        </div>
+
+        <div className="auth">
+              
+        </div>
+        </div>
+    )
+}
--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
+import { Auth } from './Auth.jsx'
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
-)
+createRoot(document.getElementById('root')).render(<Auth/>)
