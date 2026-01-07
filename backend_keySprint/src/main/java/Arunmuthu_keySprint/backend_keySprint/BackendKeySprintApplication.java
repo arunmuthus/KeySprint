9d543b810013cac432d265f7f5c8e2769f171e10
arunmuthus <arunmuthu.profiles@gmail.com>
2026-01-07 10:13:01 +0530

    backend initialized

---
+++ backend_keySprint/src/main/java/Arunmuthu_keySprint/backend_keySprint/BackendKeySprintApplication.java
@@ -0,0 +1,13 @@
+package Arunmuthu_keySprint.backend_keySprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKeySprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendKeySprintApplication.class, args);
+	}
+
+}
---
+++ backend_keySprint/src/test/java/Arunmuthu_keySprint/backend_keySprint/BackendKeySprintApplicationTests.java
@@ -0,0 +1,13 @@
+package Arunmuthu_keySprint.backend_keySprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendKeySprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
